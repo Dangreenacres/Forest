f53c0f82e9a04d7926e495ec19e74131d449411f
dangreenacres <dangreenacres@gmail.com>
2018-01-30 10:39:08 +0000

    Part implementation of MusicBean

---
+++ Forest/Forest.Data/BEANS/MusicBEAN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forest.Data.BEANS
+{
+    class MusicBEAN
+    {
+        public int Id { get; set; }
+        public string Artists { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Image_Name { get; set; }
+        public Nullable<int> Num_Tracks { get; set; }
+        public double Price { get; set; }
+        public System.DateTime Released { get; set; }
+        public string Url { get; set; }
+        public MusicBEAN() { }
+    }
+}
